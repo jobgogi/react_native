030d1603b94bf83ae5a0f6f21be947737edece6d
Jobgogi <jobgogi@Jobgogiui-MacBookPro.local>
2023-09-03 19:41:24 +0900

    Part2_Ch01_헤더 생성

--- p2_ch01_kakao-friend-list/App.js
+++ p2_ch01_kakao-friend-list/App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView } from 'react-native';
+import Header from './src/Header';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Header />
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +14,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
