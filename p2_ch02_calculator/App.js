be960d8ad094d3efb67235a287ab96355ca0022b
Jobgogi <jobgogi@Jobgogiui-MacBookPro.local>
2023-09-14 05:45:06 +0900

    계산결과 텍스트 UI개발

--- p2_ch02_calculator/App.js
+++ p2_ch02_calculator/App.js
@@ -3,12 +3,6 @@ import { StyleSheet, SafeAreaView, View } from 'react-native';
 import Calculator from './src/Calculator';
 
 export default function App() {
-  const [input, setInput] = useState(0); // 입력한 숫자
-  const [currentOperator, setCurrentOperator] = useState(null); // 계산 방식
-  const [result, setResult] = useState(null); // 결과값
-  const [tempInput, setTempInput] = useState(null); // 마지막에 입력한 숫자를 저장
-  const [tempOperator, setTempOperator] = useState(null); // 마지막 계산 방식을 저장
-
   return (
     <SafeAreaView style={styles.container}>
       <Calculator />
--- p2_ch02_calculator/src/Calculator.js
+++ p2_ch02_calculator/src/Calculator.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TouchableOpacity, View, Text } from 'react-native';
 import styled from 'styled-components/native';
 
@@ -49,10 +49,27 @@ const ButtonContainer = styled.View`
   width: 100%;
 `;
 
+const InputContainer = styled.View`
+  background-color: ${COLOR.RESULT};
+  min-height: 50px;
+  justify-content: center;
+  align-items: flex-end;
+  padding: 10px 5px;
+`;
+
 export default () => {
+  const [input, setInput] = useState(0); // 입력한 숫자
+  const [currentOperator, setCurrentOperator] = useState(null); // 계산 방식
+  const [result, setResult] = useState(null); // 결과값
+  const [tempInput, setTempInput] = useState(null); // 마지막에 입력한 숫자를 저장
+  const [tempOperator, setTempOperator] = useState(null); // 마지막 계산 방식을 저장
+
   return (
-    <View style={{ flex: 1, width: 250 }}>
+    <View style={{ flex: 1, width: 250, justifyContent: 'center' }}>
       {/* 결과 */}
+      <InputContainer>
+        <Text style={{ color: 'white', fontSize: 35, textAlign: 'right' }}>{input}</Text>
+      </InputContainer>
       {/* [AC ~ /] */}
       <ButtonContainer>
         <Button
